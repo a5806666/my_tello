22f7f39c3a96afa9d4bc2959556c5a749562149d
zhong <afs784512@hotmail.com>
2021-04-22 17:50:05 +0900

    拖拉 List(acts_as_list / railsajax)

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -12,9 +12,9 @@ ActiveStorage.start()
 
 // vue.js
 
-import Vue from 'vue/dist/vue.esm'
-import List from 'components/list'
-
+import Vue from 'vue/dist/vue.esm';
+import List from 'components/list';
+import draggable from 'vuedraggable';
 
 document.addEventListener("turbolinks:load", function(event) {
     let el = document.querySelector('#board');
@@ -25,7 +25,30 @@ document.addEventListener("turbolinks:load", function(event) {
             data: {
                 lists: JSON.parse(el.dataset.lists)
             },
-            components: { List: List }
+            components: { List: List, draggable: draggable },
+            methods: {
+                listMoved(event){
+                    console.log(event);
+
+                    let data = new FormData();
+                    data.append("list[position]", event.moved.newIndex + 1);
+                    // console.log(this.lists[event.moved.newIndex].id);
+
+                    Rails.ajax({
+                        // /lists/2/move
+                        url: `/lists/${this.lists[event.moved.newIndex].id}/move`,
+                        type: 'PUT',
+                        data: data,
+                        dataType: 'JSON',
+                        success: function(resp){
+                            console.log(resp);
+                        },
+                        error: err => {
+                            console.log(err);
+                        }
+                    });
+                }
+            }
         });
     }
 })
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :cards
-  resources :lists
+  resources :lists do 
+    member do
+      put :move # lists/2/move
+    end
+  end
 
   root 'lists#index'
 end
