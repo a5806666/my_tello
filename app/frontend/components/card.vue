7166af3d6ea23fa1a61ee16bf30abf4f7f47c9c2
zhong <afs784512@hotmail.com>
2021-04-26 16:52:11 +0900

    編輯卡片(card.vue & list.js) part 2

--- app/frontend/components/card.vue
+++ app/frontend/components/card.vue
@@ -9,7 +9,7 @@
                 <a href="#" class="closeButton" @click="close">
                     <i class="fas fa-times"></i>
                 </a>
-                <textarea class="content">{{ card.name }}</textarea>
+                <textarea class="content" v-model="card_name">}</textarea>
                 <button class="updateButton" @click="update">更新</button>
             </div> 
         </div>
@@ -17,21 +17,25 @@
 </template>
 
 <script>
+// import { mapActions } from 'vuex';
     export default {
         name: 'Card',
         data: function() {
             return {
+                card_name: this.card.name,
                 editing: false
             }
         },
         props: ['card'],
         methods: {
+            // ...mapActions(["updateCard"]),
             close(event) {
                 event.preventDefault();
                 this.editing= false;
             },
             update(event) {
                 event.preventDefault();
+                this.$store.dispatch("updateCard", {id: this.card.id, name: this.card_name})
                 this.editing= false;
             }
         }
@@ -48,7 +52,7 @@
         @apply fixed w-full h-full top-0 left-0 flex items-center justify-center;
         .inner {
             @apply bg-white flex flex-col px-6 py-2;
-            width: 700px;
+            width: 50%;
             .closeButton {
                 @apply text-gray-800 text-right;
             }
